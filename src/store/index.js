25e0ddb2d455718dda26e972c1521aa51d81a561
Ryan Delk <ryan@thedelk.com>
2021-10-22 15:48:51 -0500

    chore: add vuex-persistedstate to save tokens

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,9 @@
 import { createStore } from 'vuex'
+import persistState    from 'vuex-persistedstate'
 import auth            from './modules/auth'
 
 const store = createStore({
+  plugins: [persistState()],
   modules: { auth }
 })
 
